807e5b604557bc8cbb7f63301e9a1e4fae4b1f65
DreamVenerable <114648602+DreamVenerable@users.noreply.github.com>
2023-09-18 16:12:58 +0200

    fixed with rubocop

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,19 +1,19 @@
-def caeser_cipher (string, shift)
-  ascii = string.bytes
+# frozen_string_literal: true
+
+def caeser_cipher(str, shift)
+  ascii = str.bytes
   ascii = ascii.map do |byte|
     base = byte < 91 ? 65 : 97
     if byte.between?(65, 90) || byte.between?(97, 122)
       byte += shift
       unless byte.between?(65, 90) || byte.between?(97, 122)
-        byte = (byte - base) % (26) + base
+        byte = (byte - base) % 26 + base
         byte.chr
       end
-      byte.chr
-    else
-      byte.chr
     end
+    byte.chr
   end
-  ascii = ascii.join
+  ascii.join
 end
 
 puts 'Your message:'
@@ -21,4 +21,4 @@ message = gets.chomp
 puts 'The Number of shifts?'
 number = gets.chomp.to_i
 
-puts (caeser_cipher(message, number))
+puts(caeser_cipher(message, number))
