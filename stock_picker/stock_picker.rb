607ff74db64bb36ca323e693fd41d6279381cecc
DreamVenerable <114648602+DreamVenerable@users.noreply.github.com>
2023-09-18 16:14:59 +0200

    fixed with rubocop

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 def stock_picker(array)
-  array.combination(2).to_a.keep_if {|pair| pair[0] < pair[1]}.max_by(1) {|a, b| b - a}
+  array.combination(2).to_a.keep_if { |pair| pair[0] < pair[1] }.max_by(1) { |a, b| b - a }
 end
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
